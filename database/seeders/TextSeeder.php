46b9a67c84f63ad5bc65a3b45392f6968106dbdf
kafyar2022 <kafyar2022@gmail.com>
2022-06-20 17:00:57 +0500

    Верстает страницу О нас

--- database/seeders/TextSeeder.php
+++ database/seeders/TextSeeder.php
@@ -66,6 +66,102 @@ class TextSeeder extends Seeder
         'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec et egestas ultrices placerat id rhoncus. Sapien semper odio rhoncus, aliquam nulla quisque. Pharetra mauris commodo, varius eget sit aliquam amet.',
       ],
       [
+        'page' => 'about',
+        'slug' => 'about-vitrin-title',
+        'text' => 'Tayoldo pharma',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-vitrin-subtitle',
+        'text' => 'Молодая и перспективная компания с ключевыми компетенциями в сфере фармацевтики.
+
+Благодаря нашим инновационным продуктам мы помогаем людям с неизлечимой болезнью.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-card-description',
+        'text' => 'Мы ведем наш глобальный бизнес с максимальной честностью и прозрачностью. Мы поддерживаем
+программы, инициативы и организации, которые помогают улучшить здоровье.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-card-text',
+        'text' => 'Быстро растущее и стареющее население мира требует особого внимания в снабжении и улучшении медицинских услуг. Благодаря нашим инновационным продуктам мы вносим свой вклад в поиск решений некоторых из основных проблем нашего времени. Поскольку ожидаемая продолжительность жизни продолжает расти, мы улучшаем качество жизни растущего населения, сосредоточив наши исследования и разработки на профилактике, облегчении и лечении заболеваний.
+
+Мы стремимся работать устойчиво и выполнять
+наши социальные и этические обязательства.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-foundation-title',
+        'text' => 'Наша основа',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-foundation-subtitle',
+        'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec et egestas ultrices placerat id rhoncus. Sapien semper odio rhoncus, aliquam nulla quisque. Pharetra mauris commodo, varius eget sit aliquam amet.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-mission-title',
+        'text' => 'Миссия нашей компании ',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-mission-description',
+        'text' => 'Стать ведущей фармацевтической компанией мирового уровня, в которой будут работать сплоченные талантливые люди, занимающиеся созданием инновационных продуктов.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-vision-title',
+        'text' => 'Наше Видение',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-vision-description',
+        'text' => 'Оказывать положительное влияние на сообщества, в которых мы живем и работаем.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-values-title',
+        'text' => 'Наши ценности',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'about-values-subtitle',
+        'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec et egestas ultrices placerat id rhoncus. Sapien semper odio rhoncus, aliquam nulla quisque. Pharetra mauris commodo, varius eget sit aliquam amet.',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-1',
+        'text' => 'Здоровье',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-2',
+        'text' => 'Инновации',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-3',
+        'text' => 'Сотрудники',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-4',
+        'text' => 'Сплоченность',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-5',
+        'text' => 'Общество',
+      ],
+      [
+        'page' => 'about',
+        'slug' => 'values-item-6',
+        'text' => 'Здоровье',
+      ],
+      [
         'page' => '',
         'slug' => '',
         'text' => '',
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,64 @@
 @section('title', 'О нас | Tayoldo')
 
 @section('content')
-  <main class="content">
-    <div class="container">About page</div>
+  <main class="container content">
+    <div class="vitrin about__vitrin">
+      <div class="vitrin__inner">
+        <h1 class="title vitrin__title">{{ $data['texts']['about-vitrin-title'] }}</h1>
+        <p class="txt subtitle">{{ $data['texts']['about-vitrin-subtitle'] }}</p>
+      </div>
+    </div>
+
+    <div class="about-card">
+      <p class="about-card__description">{{ $data['texts']['about-card-description'] }}</p>
+      <p class="txt about-card__text">{{ $data['texts']['about-card-text'] }}</p>
+    </div>
+
+    <section class="foundation">
+      <h2 class="title">{{ $data['texts']['about-foundation-title'] }}</h2>
+      <p class="txt subtitle">{{ $data['texts']['about-foundation-subtitle'] }}</p>
+
+      <div class="foundation__inner">
+        <section class="about-mission">
+          <div class="mission-vision__inner">
+            <h3 class="txt mission-vision-title">{{ $data['texts']['about-mission-title'] }}</h3>
+            <p class="txt mission-vision-description">{{ $data['texts']['about-mission-description'] }}</p>
+          </div>
+        </section>
+
+        <section class="about-vision">
+          <div class="mission-vision__inner">
+            <h3 class="txt mission-vision-title">{{ $data['texts']['about-vision-title'] }}</h3>
+            <p class="txt mission-vision-description">{{ $data['texts']['about-vision-description'] }}</p>
+          </div>
+        </section>
+      </div>
+    </section>
+
+    <section class="values">
+      <h2 class="title values__title">{{ $data['texts']['about-values-title'] }}</h2>
+      <p class="txt subtitle">{{ $data['texts']['about-values-subtitle'] }}</p>
+
+      <ul class="value-list">
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-1'] }}</h3>
+        </li>
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-2'] }}</h3>
+        </li>
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-3'] }}</h3>
+        </li>
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-4'] }}</h3>
+        </li>
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-5'] }}</h3>
+        </li>
+        <li class="value-item">
+          <h3 class="value-item__title">{{ $data['texts']['values-item-6'] }}</h3>
+        </li>
+      </ul>
+    </section>
   </main>
 @endsection
